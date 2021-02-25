da16c3133a94b628d05ca41c30253c06b750ce41
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-02-25 19:34:12 +0900

    Add exercise

---
+++ day07/src/jp/freestyles/day/six/PositiveNegativeChecker.java
@@ -0,0 +1,22 @@
+package jp.freestyles.day.six;
+
+import java.util.Scanner;
+
+public class PositiveNegativeChecker {
+
+    public static void main(String[] args) {
+
+        Scanner stdin = new Scanner(System.in);
+
+        System.out.print("正数または負数を入力してください: ");
+        int num = stdin.nextInt();
+
+        if (num > 0) {
+            System.out.println("入力値は正数です");
+        } else {
+            System.out.println("入力値は負数です");
+        }
+
+        stdin.close();
+    }
+}
