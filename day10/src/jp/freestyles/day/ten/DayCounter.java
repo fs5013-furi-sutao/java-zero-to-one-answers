8c59f6e0c781076654e98853f868e6164c39b296
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-02-25 21:26:53 +0900

    Add exercise

---
+++ day10/src/jp/freestyles/day/ten/DayCounter.java
@@ -0,0 +1,13 @@
+package jp.freestyles.day.ten;
+
+public class DayCounter {
+
+    public static void main(String[] args) {
+
+        final int END_DAY = 27;
+
+        for (int i = 1; i <= END_DAY; i++) {
+            System.out.format("%d 日目 %n", i);
+        }
+    }
+}
