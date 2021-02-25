bd01ad161ea83b28a185f8198fc8f781ff5e1cbf
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-02-25 12:05:24 +0900

    First commit

---
+++ day01/src/day/one/print/Printer.java
@@ -0,0 +1,12 @@
+package day.one.print;
+
+public class Printer {
+
+    public static void main(String[] args) {
+        String myName = "山本一郎";
+        int myAge = 757;
+
+        System.out.println("私は " + myName + " です");
+        System.out.println("年齢は " + myAge + " 歳です");
+    }
+}
