3872f3cd707542e186ecfd2a49e9791786d4f04e
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-02-25 15:41:58 +0900

    Add exercise

---
+++ day05/src/jp/freestyles/day/five/StringInputReciever.java
@@ -0,0 +1,17 @@
+package jp.freestyles.day.five;
+
+import java.util.Scanner;
+
+public class StringInputReciever {
+
+    public static void main(String[] args) {
+
+        Scanner stdin = new Scanner(System.in, "Shift_JIS");
+
+        System.out.print("名前を入力してください: ");
+        String name = stdin.nextLine();
+
+        System.out.format("入力された名前は %s です", name);
+        stdin.close();
+    }
+}
