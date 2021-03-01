f8b3508e6e95f1c131831429c793363f5dae3756
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-03-01 17:30:13 +0900

    Add exercise

---
+++ day18/src/jp/freestyles/day/eighteen/StaticMethodAdder.java
@@ -0,0 +1,17 @@
+package jp.freestyles.day.eighteen;
+
+public class StaticMethodAdder {
+
+    public static void main(String[] args) {
+
+        int a = 57;
+        int b = 25;
+
+        int result = add(a, b);
+        System.out.format("%d + %d = %d %n", a, b, result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
