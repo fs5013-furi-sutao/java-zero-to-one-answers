6a91a7aa6a4800b70f6706a6e09fcf3e922f8b2e
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-03-03 13:26:18 +0900

    Add exercise

---
+++ day29/src/jp/freestyles/day/eleven/DayCounter.java
@@ -0,0 +1,19 @@
+package jp.freestyles.day.eleven;
+
+public class DayCounter {
+
+    public static void main(String[] args) {
+
+        final int NUMBER_OF_COLOUMN = 9;
+        final int NUMBER_OF_ROW = 9;
+
+        for (int i = 1; i <= NUMBER_OF_COLOUMN; i++) {
+            for (int j = 1; j <= NUMBER_OF_ROW; j++) {
+
+                System.out.format(
+                    "%d X %d = %2d, ", i, j, i * j);
+            }
+            System.out.println();
+        }
+    }
+}
